9913a7729e5fad41eaf3c11722e2443d527ede4f
Punit Naik <naik.punit44@gmail.com>
2015-06-19 12:35:40 +0530

    Update MatMulMap.java

--- MatrixMultiplication_MapReduce/src/MatMulMap.java
+++ MatrixMultiplication_MapReduce/src/MatMulMap.java
@@ -17,7 +17,7 @@ public class MatMulMap extends Mapper<LongWritable, Text, Text, Text> {
 		Text Value = new Text();
 		String line = value.toString();
 		String[] val = line.split(",");
-		if(val[0]=="A"){
+		if(val[0].contains("A")){
 			for(int x=0;x<p;x++){
 				String s="";
 				Key.set(val[(n+1)]+","+x);
