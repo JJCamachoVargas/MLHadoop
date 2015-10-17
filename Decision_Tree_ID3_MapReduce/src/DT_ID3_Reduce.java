6fad0724e150e47180b209edd335f9f5b3f37ed8
Punit Naik <naik.punit44@gmail.com>
2015-10-17 11:15:49 +0530

    Update DT_ID3_Reduce.java

--- Decision_Tree_ID3_MapReduce/src/DT_ID3_Reduce.java
+++ Decision_Tree_ID3_MapReduce/src/DT_ID3_Reduce.java
@@ -2,6 +2,10 @@ import java.io.IOException;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+/* NOTE!!: Right now I have just written all the trees that are generated on the various distributed data blocks.
+	   I Did not add the code for sort of averaging out all the decision trees and select only one tree because
+	   I did not have time.
+*/
 public class DT_ID3_Reduce extends Reducer<Text, Text, Text, Text>{
 	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
 			for(Text value:values){
