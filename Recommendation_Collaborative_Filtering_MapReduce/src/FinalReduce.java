81809f8ff6af17f06e31e184ee1553bc63fcdb25
Punit Naik <naik.punit44@gmail.com>
2015-10-20 10:53:03 +0530

    Update FinalReduce.java

--- Recommendation_Collaborative_Filtering_MapReduce/src/FinalReduce.java
+++ Recommendation_Collaborative_Filtering_MapReduce/src/FinalReduce.java
@@ -3,17 +3,14 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 
-public class FinalReduce extends Reducer<Text, Text, Text, Text> {
-	public static String delimiter=null;
-	public static String identifier=null;
+public class FinalReduce extends Reducer<Text, Text, Text, Text>{
 	@Override
-	public void setup(Context context){
-		delimiter=context.getConfiguration().get("delimiter");
-		identifier=context.getTaskAttemptID().getTaskID().getId()+delimiter;
-	}
-	@Override
-	public void reduce(Text key, Iterable<Text> values, Context context)
-			throws IOException, InterruptedException {
+	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
+		String delimiter=null, identifier=null;
+		if(delimiter==null && identifier==null){
+			delimiter=context.getConfiguration().get("delimiter");
+			identifier=context.getTaskAttemptID().getTaskID().getId()+delimiter;	
+		}
 		for(Text val: values){
 			context.write(new Text(/*identifier+*/key.toString()), val);//new Text(val.toString().split("\\,")[1]));
 		}
