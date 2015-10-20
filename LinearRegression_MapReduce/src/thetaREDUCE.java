d1ca00265d2816c7a009fe657edafff9fad29420
Punit Naik <naik.punit44@gmail.com>
2015-10-20 10:23:15 +0530

    Update thetaREDUCE.java

--- LinearRegression_MapReduce/src/thetaREDUCE.java
+++ LinearRegression_MapReduce/src/thetaREDUCE.java
@@ -3,7 +3,7 @@ import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class thetaREDUCE extends Reducer<Text, FloatWritable, Text, FloatWritable> {
+public class thetaREDUCE extends Reducer<Text, FloatWritable, Text, FloatWritable>{
 	public void reduce(Text key, Iterable<FloatWritable> values, Context context) throws IOException, InterruptedException{
 		float sum=0;
 		int count=0;
