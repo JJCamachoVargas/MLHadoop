1ccca0f6659e96139d83290104254db40f729ecd
Punit Naik <naik.punit44@gmail.com>
2015-10-23 16:39:55 +0530

    Update KMeansClassificationReduce.java

--- KMeansClustering_MapReduce/src/KMeansClassificationReduce.java
+++ KMeansClustering_MapReduce/src/KMeansClassificationReduce.java
@@ -3,7 +3,6 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class KMeansClassificationReduce extends Reducer<Text, Text, Text, Text>{
-
 	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
 		for(Text val:values){
 			context.write(key,val);
