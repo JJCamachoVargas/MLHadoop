422256a9f929eb7ac20d5e05c581ec1c5f263b9e
Punit Naik <naik.punit44@gmail.com>
2015-10-23 16:39:43 +0530

    Update KMeansCentroidCalculationReduce.java

--- KMeansClustering_MapReduce/src/KMeansCentroidCalculationReduce.java
+++ KMeansClustering_MapReduce/src/KMeansCentroidCalculationReduce.java
@@ -4,7 +4,6 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class KMeansCentroidCalculationReduce extends Reducer<Text, Text, Text, Text>{
-
 	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException{
 		int count=0;
 		float x_sum=0, y_sum=0;
